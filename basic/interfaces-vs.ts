96ca53a6355834c6f04262c3a2a2b40c983bf67e
Leandro Soares <lsoares199658@gmail.com>
2020-05-28 13:10:18 -0300

    interfaces vs type

---
+++ basic/interfaces-vs.ts
@@ -0,0 +1,50 @@
+// Interfaces
+
+// definition
+interface Game {
+  title: string;
+}
+
+interface DLC {
+  extra: string;
+}
+
+// intersection | extend
+interface GameCollection extends Game, DLC {}
+
+// implements
+class CreateGame implements GameCollection {}
+
+// declare function
+interface getSimilars {
+  (title: string): void;
+}
+
+// ============== Diff ============== //
+
+interface ID extends number {}
+
+// not allowed define Tuples on interface
+interface Tuple {
+  0: number;
+  1: number;
+}
+
+[1, 2] as Tuple;
+
+// can have multiple declarations and it joins the same name
+
+interface JQuery {
+  a: string;
+}
+
+interface JQuery {
+  b: string;
+}
+
+const $: JQuery = {
+  a: "bla",
+  b: "foo",
+};
+
+// advantages: when creating libs, prefer Interface because it's more extensives
