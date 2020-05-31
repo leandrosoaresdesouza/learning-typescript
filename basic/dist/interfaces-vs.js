29aabef3b76be84da41231e23a12a79d2ce54dfa
Leandro Soares <lsoares199658@gmail.com>
2020-05-31 17:51:01 -0300

    interface vs types

---
+++ basic/dist/interfaces-vs.js
@@ -0,0 +1,15 @@
+"use strict";
+// Interfaces
+// implements
+var CreateGame = /** @class */ (function () {
+    function CreateGame() {
+    }
+    return CreateGame;
+}());
+[1, 2];
+var $ = {
+    a: "bla",
+    b: "foo",
+};
+// advantages: when creating libs, prefer Interface because it's more extensives!
+// creating objects/classes (POO)
---
+++ basic/dist/type-vs.js
@@ -0,0 +1,13 @@
+"use strict";
+// Type Alias
+// implements
+var CreateGameT = /** @class */ (function () {
+    function CreateGameT() {
+    }
+    return CreateGameT;
+}());
+[1, 2];
+// recommend:
+// mostly
+// React - Props
+// CONSISTENCY!
